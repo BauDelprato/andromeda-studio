4c7095527b46908e3cfbb64e3bf76d1ca4312cd7
Bau <bautistadelprato16@gmail.com>
2026-09-14 16:15:48 -0300

    feat: add crew management endpoints

---
+++ Backend/Andromeda.Api/Andromeda.Api/DTOs/Crews/CrewResponse.cs
@@ -0,0 +1,13 @@
+﻿using Andromeda.Api.Models;
+
+namespace Andromeda.Api.DTOs.Crews
+{
+    public class CrewResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public CrewLevel Level { get; set; }
+        public CrewAge Age { get; set; }
+        public CrewSize Size { get; set; }
+    }
+}
---
+++ Backend/Andromeda.Api/Andromeda.Api/DTOs/Crews/UpdateCrewRequest.cs
@@ -0,0 +1,12 @@
+﻿using Andromeda.Api.Models;
+
+namespace Andromeda.Api.DTOs.Crews
+{
+    public class UpdateCrewRequest
+    {
+        public string? Name { get; set; }
+        public CrewLevel? Level { get; set; }
+        public CrewAge? Age { get; set; }
+        public CrewSize? Size { get; set; }
+    }
+}
